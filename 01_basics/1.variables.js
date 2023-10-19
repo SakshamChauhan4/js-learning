81790b19b71cd9a8a411ba690b8c3642c3f2abb1
SakshamChauhan4 <sakshamvchauhan4@gmail.com>
2023-10-19 19:49:46 +0530

    lecture on Variables

---
+++ 01_basics/1.variables.js
@@ -0,0 +1,18 @@
+//constant keyword
+const accountId = 144553;
+let accountEmail = "Saksham@gmail.com";
+var accountPassword = "123";
+let accountState;
+
+//also use let always
+accountCity = "Delhi";
+
+
+
+/* cant change because of consta keyword 
+accountId = 2 */
+
+
+/* Prefer not to use var because of of issue in block scope and functional scope */
+
+console.table([accountEmail,accountId,accountState]);
