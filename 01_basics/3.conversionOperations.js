52278a8f2937609988b8ef077c15308defd77a21
SakshamChauhan4 <sakshamvchauhan4@gmail.com>
2023-10-19 22:06:32 +0530

    operations

--- 01_basics/3.conversionOperations.js
+++ 01_basics/3.conversionOperations.js
@@ -23,5 +23,36 @@ let booleanIsLoggedIn = Boolean(isLoggedIn);
 let someNumber = 33;
 let stringNumber = String(someNumber);
 
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// *************************************** Operations ***************************************
+
+let value = 3;
+let negValue = -value;
+// console.log(negValue);
+
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = "hello ";
+let str2 = "saksham";
+let str3 = str1 + str2;
+// console.log(str3);
+
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log(+true); it is converted to number and then value is increased
+
+let gameCounter = 100;
+++gameCounter;
+console.log(gameCounter);
+
+// link to study
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
