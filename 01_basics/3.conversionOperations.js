f9315e19f64a4035b4201957e31cf888dcd17609
SakshamChauhan4 <sakshamvchauhan4@gmail.com>
2023-10-19 21:51:12 +0530

    conversion of datatypes

---
+++ 01_basics/3.conversionOperations.js
@@ -0,0 +1,27 @@
+let score = "Saksham";
+
+// console.log(typeof score); // it is as a variable
+// console.log(typeof (score)); // it is used as a method
+
+let valueInNumber = Number(score);
+// console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN (not a number)
+// true => 1
+// false => 0
+
+let isLoggedIn = "";
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+// 1 => true; 0=> false
+// " " => false
+// "anything written" => true
+
+let someNumber = 33;
+let stringNumber = String(someNumber);
+
+console.log(stringNumber);
+console.log(typeof stringNumber);
