8628e743d31275154f6814a31a189e92f5d2f024
SakshamChauhan4 <sakshamvchauhan4@gmail.com>
2023-10-19 20:06:06 +0530

    datatypes

---
+++ 01_basics/2.datatypes.js
@@ -0,0 +1,21 @@
+"use strict"; //treat all JS code as newer version(this means the more modern version of js)
+
+// alert(3+3); since we are using node js, not browser
+
+let age = 18;
+console.log(3+3);
+console.log("use a new line since it is a better practise");
+
+// number => 2 to pwer 53
+// bigint
+// string => ""
+// boolean => true/false 
+// null => standalone value
+// undefined => 
+// symbol => uniqueness
+
+// object 
+
+console.log(typeof age);
+console.log(typeof null);
+console.log(typeof undefiend);
