4094c934ebb4e04bab3c8e7a016e4ec3616bf853
SakshamChauhan4 <sakshamvchauhan4@gmail.com>
2023-10-19 22:16:44 +0530

    comparisions

---
+++ 01_basics/4.comparision.js
@@ -0,0 +1,20 @@
+console.log(2 > 1);
+console.log(2 >= 1);
+console.log(2 == 1);
+console.log(2 != 1);
+console.log(2 < 1);
+console.log(2 <= 1);
+
+console.log("02" > 1); 
+//javascript automatically convert it to numbers
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+console.log(null < 0);
+console.log(null <= 0);
+
+console.log(undefined == 0);
+
+// === is strict check it checks the value as well as datatypes
+console.log("2" === 2);
