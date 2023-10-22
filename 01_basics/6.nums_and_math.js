8dc4b34170aae06b23512990d2a6356e03ceb3b3
SakshamChauhan4 <sakshamvchauhan4@gmail.com>
2023-10-22 22:15:41 +0530

    nums

---
+++ 01_basics/6.nums_and_math.js
@@ -0,0 +1,19 @@
+const score = 400;
+
+const balance = new Number(100);
+
+// console.log(score);
+// console.log(balance); 
+// prototype is number which has less values than string
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(1));
+// console.log(typeof balance);
+
+const otherNumber = 1123.8966;
+// console.log(otherNumber.toPrecision(4));
+
+const hundreds = 1000000;
+// console.log(hundreds.toLocaleString('en-IN')); // to convert to indian system or you can use any code and convert it to any country's code
+
+console.log(Math);
